e546c9856b92be802515a5f1fb4a45f3fdffc0b6
何政文 <hzw122345@gmail.com>
2022-09-07 16:08:03 +0800

    1

---
+++ deploy.bat
@@ -0,0 +1,12 @@
+cd \
+cd C:\_Projects\BlueMix\git\spring-boot-security-saml
+call mvn clean
+call mvn package -U
+cf logout
+cf login -u David.campbell@ontario.ca -p Meaghan1!!** -a https://api.ops1.ca-east.bluemix.net -o OPS-CAC-Dev1 -s "CAC-POC Functional Tests"
+cd target
+call cf push DCJavaSpringSaml2Sample -p spring-boot-security-saml-sample-1.4.0.RELEASE.war
+IF "%1"=="logs" (call cf logs DCJavaSpringSaml2Sample) 
+cd ..
+
+
